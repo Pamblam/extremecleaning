b584536dbeede9115bac873a338cf38419d6b04a
Pamblam <adelphia@protonmail.ch>
2023-11-19 00:34:08 -0500

    initial commit

---
+++ index.php
@@ -0,0 +1,368 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <title>Hoarder cleanup, removal and Extreme cleaning, Hoarder clean outs at Florida, Tampa FL</title>
+
+<meta name="description" content="Floridahoardercleanup.com offers Hoarder cleanup, removal and Extreme cleaning, Hoarder clean outs at Florida, Tampa FL"/>
+
+<meta name="keywords" content="Hoarder cleanup Florida, Hoarder removal Florida, Extreme cleaning Florida, Hoarder clean outs Florida, Hoarder cleanup Tampa FL, Hoarder removal Tampa FL, Extreme cleaning Tampa FL, Hoarder clean outs Tampa FL, Hoarder cleanup Clearwater FL, Hoarder removal Clearwater FL, Extreme cleaning Clearwater FL, Hoarder clean outs Clearwater FL, Hoarder cleanup St Petersburg FL, Hoarder removal St Petersburg FL, Extreme cleaning St Petersburg FL, Hoarder clean outs St Petersburg FL"/>
+
+
+
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <!-- Loading Bootstrap -->
+    <link href="bootstrap/css/bootstrap.css" rel="stylesheet">
+    <link href="css/style.css" rel="stylesheet">
+    <link href="css/color.css" rel="stylesheet">
+    <link href="css/responsive.css" rel="stylesheet">
+    <link href="css/lightbox.css" rel="stylesheet" />
+    <!-- Font Awesome -->
+    <link href="css/font-awesome.css" rel="stylesheet">
+
+    <!-- HTML5 shim, for IE6-8 support of HTML5 elements. All other JS at the end of file. -->
+    <!--[if lt IE 9]>
+      <script src="js/html5shiv.js"></script>
+      <script src="js/respond.min.js"></script>
+    <![endif]-->
+    <script src="js/jquery-1.11.0.min.js"></script>
+    <script type="text/javascript">
+			$(function(){
+    $(".dropdown").hover(            
+            function() {
+                $('.dropdown-menu', this).stop( true, true ).fadeIn("fast");
+                $(this).toggleClass('open');
+                $('b', this).toggleClass("caret caret-up");                
+            },
+            function() {
+                $('.dropdown-menu', this).stop( true, true ).fadeOut("fast");
+                $(this).toggleClass('open');
+                $('b', this).toggleClass("caret caret-up");                
+            });
+    });
+</script>  
+<script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-100554757-2', 'auto');
+  ga('send', 'pageview');
+
+</script>
+
+    
+</head>
+<body>
+    <div class="item">
+				<div class="topbar-6 padding-bottom-20 padding-top-20" id="top-bar">
+					
+						<div class="container">
+								<div class="col-md-3 col-sm-3 col-xs-12 header6-logo padding-0">
+									<a href="index.html" class="navbar-brand"><img alt="" src="images/logo.png"></a>
+								</div>	
+								<div class="col-md-4 col-sm-3 col-xs-12 top-phone text-center padding-0" style="padding-top:14px;">
+                                <h3 style="margin:0px; color:#0057ae;">Our Address</h3>
+									<p><i class="fa fa-map-marker"></i> 8326 17th St., St. Petersburg, FL 33702</p>	
+								</div>	
+                                	<div class="col-md-4 col-sm-3 col-xs-12 top-phone text-center padding-0" style="padding-top:14px;">
+                                <h3 style="margin:0px; color:#0057ae; text-align:center;">Call Us</h3>
+									<p><i class="fa fa-phone"></i> 727-557-7086</p>	
+								</div>	
+								<div class="col-md-1 col-sm-3 col-xs-12 topbar6-social padding-0" style="padding-top:20px;">
+										<a href="https://www.facebook.com/" target="_blank"><i class="fa fa-facebook"></i></a>
+										<a href="https://twitter.com/?lang=en" target="_blank"><i class="fa fa-twitter"></i></a>
+										<a href="https://plus.google.com/" target="_blank"><i class="fa fa-google"></i></a>
+										
+								</div>
+							
+						</div>
+					
+					
+				</div><!-- /.item -->
+				<div class="header4 padding-bottom-0 background-color-dark" id="header">
+			
+					<div class="wrapper">
+				
+						<div class="container">
+					
+							<nav role="navigation" class="navbar plain">
+								<div class="navbar-header">
+									<button data-target="#navbar-collapse-02" data-toggle="collapse" class="navbar-toggle" type="button">
+										<span class="sr-only">Toggle navigation</span>
+									</button>
+									
+									</div>
+											
+								<div id="navbar-collapse-02" class="collapse navbar-collapse">
+									<ul class="nav navbar-nav navbar-left">			      
+										<li class="active propClone"><a href="index.html" style="color:#fff!important; border-bottom:1px solid #fff; padding-bottom:10px;">Home</a></li>
+                                        
+                                        <li class="propClone dropdown menu-link"><a href="#" class="dropdown-toggle" data-toggle="dropdown" style="background:none;color:#fff!important;"> Services <span class="caret"></span></a>
+                                	 		<ul class="dropdown-menu">
+                                         		<li class="propClone"><a href="sucide.html" style="color:#0e7f0b!important;">Suicide,Homicide,Unattended Death Cleanup</a></li>
+                                      			<li class="propClone"><a href="hoarding.html" style="color:#0e7f0b!important;">Hoarding Cleanup</a></li>
+                                                <li class="propClone"><a href="deep_cleaning.html" style="color:#0e7f0b!important;">Deep Cleanings of Homes and Businesses</a></li>
+                                      			<li class="propClone"><a href="rodents.html" style="color:#0e7f0b!important;">Rodent Infestation and Advanced Odor Treatment </a></li>
+                                                
+                                            </ul>
+                                		</li>
+										
+										
+										
+										<li class="propClone"><a href="photo-gallery.html" style="color:#fff!important;">Photo Gallery </a></li>
+									
+										<li class="propClone"><a href="contact-us.html" style="color:#fff!important;">Contact Us</a></li>
+									</ul> 		      
+								</div><!-- /.navbar-collapse -->
+							</nav>
+					
+						</div><!-- /.container -->
+				
+					</div><!-- /.wrpaper -->
+				</div>
+                
+           <div id="myCarousel" class="carousel slide" data-ride="carousel">
+  <!-- Indicators -->
+  <ol class="carousel-indicators">
+    <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+    <li data-target="#myCarousel" data-slide-to="1"></li>
+  
+  </ol>
+
+  <!-- Wrapper for slides -->
+  <div class="carousel-inner">
+    <div class="item active">
+      <img src="images/slider1.png" width="100%">
+    </div>
+
+    <div class="item">
+      <img src="images/slider2.png" width="100%">
+    </div>
+
+  
+  </div>
+ 
+</div>
+        
+        <div class="item padding-top-74" id="aboutus3">
+				
+						<div class="title text-center">
+								<h1 style="font-size:35px;">Welcome to <span>EXTREME CLEANING & MORE!</span></h1>
+								<div class="title-border background-color-blue"></div>
+								<div class="col-md-9 sub-title">
+									<p>Extreme Cleaning & More is a locally owned and operated business that is fully insured and licensed in the state of Florida. We specialize in disaster clean-up (suicide, crime scene and hoarding). We have an outstanding reputation but what is most important: We will work with you within your budgetary limitations.</p>
+
+<p>My staff and myself go the extra steps to make sure that you and your family are completely satisfied with our services. Please call Darryl at (727) 557-7086 for your FREE estimate.</p>
+
+<p>When a biohazard occurs in your home or business, you need professional biohazard cleanup, and you need it now, we will be there. We are available 24/7 and we always use unmarked vehicles for your discretion.</p>
+
+<p>Extreme Cleaning & More is a company that is focused on helping people at the most difficult times.</p>
+
+<p>We are a locally owned company that has been in operation for over 10 years. We service all of Tampa Bay.</p>
+
+<p>We're on call 24 hours a day, 7 days a week, because your property and peace of mind depends on it.</p>
+								</div>
+						</div>
+		</div>
+        
+        <div class="item padding-top-0 padding-bottom-50" id="aboutus3">
+					<div class="container">
+						<div class="title">																							       
+                        
+               <div class="col-md-6 text-center"> 
+                       <div class="row border-line">
+                       <a href="gallery/gallery1.jpg" rel="lightbox[plants]" ><img src="gallery/gallery1.jpg" alt=""/ class="gallery"></a> 
+                       <a href="gallery/gallery2.jpg" rel="lightbox[plants]" ><img src="gallery/gallery2.jpg" alt=""/ class="gallery"></a>
+                       </div>
+               </div>
+        
+                <div class="col-md-6 text-center">
+                        <div class="row border-line">
+                        <a href="gallery/gallery3.jpg" rel="lightbox[plants]" ><img src="gallery/gallery3.jpg" alt=""/ class="gallery"></a> 
+                        <a href="gallery/gallery4.jpg" rel="lightbox[plants]" ><img src="gallery/gallery4.jpg" alt=""/ class="gallery"></a>    
+                         </div>
+                 </div>
+     
+    
+						</div>
+		</div>
+        </div>
+        
+        
+        <div class="item padding-top-50 padding-bottom-50" id="aboutus6">
+				
+						<div class="title text-center">
+								<h1 style="font-size:30px;"><span>Biohazard CleanupServices Include (but not limited to):</span></h1>
+								<div class="title-border background-color-blue"></div>
+								
+						</div>	
+						
+					<div class="our-expertise44 expert-tabs">
+						<div class="container">
+								
+                            
+                            <div class="col-md-4 text-center">
+                            	<h2><i class="fa fa-check-circle"></i></h2><a href="sucide.html">Suicide,Homicide,<br>Unattended Death Cleanup</a><p>(Including Blood and Bodily Fluid Spills)</p>
+										
+								
+                            </div>
+                            
+                             <div class="col-md-4 text-center">
+                            	<h2><i class="fa fa-check-circle"></i></h2><a href="hoarding.html">Hoarding Cleanup</a>
+                              
+                            </div>
+                            
+                            <div class="col-md-4 text-center">
+                            	<h2><i class="fa fa-check-circle"></i></h2><a href="deep_cleaning.html">Deep Cleanings of Homes <br>and Businesses</a>
+                            </div>
+                            
+                              
+                            <div class="col-md-12 text-center">
+                            	<h2><i class="fa fa-check-circle"></i></h2><a href="rodents.html">Rodent Infestation and Advanced<br> Odor Treatments </a>
+                            </div>
+						</div>
+					</div>
+					<div class="clearfix"></div>
+                    
+                    
+		
+		</div>
+        
+        <div class="item padding-top-50 padding-bottom-50" id="callout2">
+			<div class="callout2-line margin-top-0">
+				<div class="lie"></div>
+				<div class="cal2-icon"><i class="fa fa-star"></i></div>	
+				<div class="lie"></div>
+				
+			</div>
+			<div class="clearfix"></div>
+            <h3 class="color-white">EXTREME CLEANING & MORE</h3>
+			<h1 style="font-size:30px;">The Affordable Way</h1>
+<h2>PHONE CALLS ONLY PLEASE!!<br>
+
+24/7 Local Response</h2>
+
+<h1 style="font-size:30px;">727-557-7086</h1>
+</div>
+
+
+        
+		</div>
+        
+        <div class="item">
+			
+				
+					
+				<footer class="padding-top-74 padding-bottom-74" id="footer" style="background:#3d3d3d;">
+					<div class="container">
+                    
+                    <div class="col-md-4 col-sm-4 col-xs-12 padding-left-0 widgets">
+							<div class="wid-title">
+								<h3>QUICK LINKS</h3>
+							</div>
+							<div class="quick-links">
+								<a href="index.html">Home</a></li>
+										<a href="sucide.html">Suicide,Homicide,Unattended Death Cleanup</a>
+									
+									
+										<a href="contact-us.html">Contact Us</a>
+							</div>
+							
+						</div>
+						<div class="col-md-3 col-sm-3 col-xs-12 padding-left-0 widgets">
+							<div class="wid-title">
+								<h3>Contact Us</h3>
+							</div>
+							<p><strong>Extreme Cleaning & More</strong></p>
+                            <p><i class="fa fa-map-marker"></i> Address: 8326 17th St., St. Petersburg, FL 33702</p>
+<p><i class="fa fa-phone"></i> Phone: 727-557-7086</p>
+<p><i class="fa fa-clock-o"></i> Hours: Monday – Sunday<br> 12am – 11:30pm</p>
+						</div>
+                        
+                        <div class="col-md-2 col-sm-2 col-xs-12 padding-left-0 widgets">
+							<div class="wid-title">
+								<h3>Follow Us</h3>
+							</div>
+							
+							<div class="footer6-social">
+								<a href="https://www.facebook.com/" target="_blank"><i class="fa fa-facebook"></i></a>
+										<a href="https://twitter.com/?lang=en" target="_blank"><i class="fa fa-twitter"></i></a>
+										<a href="https://plus.google.com/" target="_blank"><i class="fa fa-google"></i></a>
+								
+							</div>
+						</div>
+						
+                        	<div class="col-md-3 col-sm-3 col-xs-12 padding-left-0 widgets">
+							<div class="wid-title">
+								<h3>Our Location</h3>
+							</div>
+							<iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d28221.858353405583!2d-82.65589!3d27.848767!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0xb1e707fd8b7229c0!2sExtreme+Cleaning+%26+More!5e0!3m2!1sen!2sin!4v1466081798834" width="100%" height="200" frameborder="0" style="border:0" allowfullscreen></iframe>
+						</div>
+						
+						
+					</div>
+					
+				
+				</footer>
+				<div class="contact6-right padding-top-30 padding-bottom-30">
+					<div class="container">
+						
+						<p>© 2022 EXTREME CLEANING & MORE. ALL RIGHTS RESERVED</p>
+						
+					</div>
+				</div>
+		
+		</div>
+        
+        
+	 <script src="js/jquery.js"></script>
+	 <script src="js/bootstrap.min.js"></script>
+	<script src="js/carousel/jquery.sly.js"></script>
+	<script src="js/carousel/plugins.js"></script>
+	<script src="js/carousel/main.js"></script>
+	
+    <script src="js/carousel/triger.js"></script>
+	<script src="js/owl.carousel.js"></script>
+	<script>
+		$(document).ready(function() {	
+		"use strict";
+		$("#owl-testimonial-1").owlCarousel({
+		autoPlay: true, //Set AutoPlay to 3 seconds
+		
+      items : 1,
+	navigation : true
+  });
+	
+ }); 
+	</script>
+	<script src="js/viedobox/video.js"></script>
+	<script src="js/modernizr.custom.js"></script>
+	 <script src="js/lightbox-2.6.min.js"></script>
+	<script src="js/toucheffects.js"></script>
+	<script src="js/history/jquery-ui.min.js"></script>
+	<script src="js/history/simple-timeline.jquery.js"></script>
+	<script>
+
+		$('div.tlcontainer').simpleTimeline();
+  
+	</script>
+	<script src="js/migrate.js"></script>
+	<script src="js/carousel/slick.min.js"></script>
+	<script>
+	$('.class').slick({
+  infinite: true,
+  slidesToShow: 3,
+  slidesToScroll: 3
+	});
+	</script>
+	<script type="text/javascript" src="js/jquery.fancybox.pack8cbb.js?v=2.1.5"></script>
+	<script src="js/paper-collapse.js"></script>
+	<script type="text/javascript" src="js/goalProgress.js"></script>
+	 <script type="text/javascript" src="js/sorting.js"></script>
+	<script src="js/application.js"></script>
+	
+</body>
+</html>
